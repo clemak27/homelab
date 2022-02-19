d605348bc1deb596b3c413acf455cd6cd5deeddb
clemak27 <clemak27@mailbox.org>
2022-02-19 09:00:07 +0100

    feat(general): Init homelab repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# my homelab setup
+
+This repo contains my dotfiles + setup for my homlab
+
+## What's in this repo?
+
+stuff.
---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix.url = github:Mic92/sops-nix;
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, home-manager, sops-nix, flake-utils }:
+    let
+      devpkgs = nixpkgs.legacyPackages.x86_64-linux;
+      updateServers = nixpkgs.legacyPackages.x86_64-linux.writeShellScriptBin "update-flake" ''
+        echo "Updating flake"
+        nix flake update --commit-lock-file --commit-lockfile-summary "chore(flake): Update $(date -I)"
+
+        echo "Pushing update to server"
+        git push
+      '';
+
+    in
+    {
+      nixosConfigurations = {
+        snowflake = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            home-manager.nixosModules.home-manager
+            sops-nix.nixosModules.sops
+            ./snowflake/configuration.nix
+          ];
+        };
+
+      };
+
+      devShell.x86_64-linux = devpkgs.mkShell {
+        nativeBuildInputs = with devpkgs; [
+          dconf2nix
+          sops
+          age
+          ssh-to-age
+          updateServers
+        ];
+      };
+
+    };
+}
