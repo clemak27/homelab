67fb86feef46d4aa629956a40fbf189de50b000c
clemak27 <clemak27@mailbox.org>
2022-03-06 00:00:10 +0100

    chore(snowflake): Add hostname to gitops script

--- snowflake/gitops-upgrade.sh
+++ snowflake/gitops-upgrade.sh
@@ -6,11 +6,11 @@ DIFF=$(cmp /tmp/git_tree_old /tmp/git_tree_new)
 if [ "$DIFF" != "" ]
 then
   cd /home/clemens/Projects/homelab || exit
-  curl -X POST -H "Content-Type: application/json" -d '{text": "Starting system update."}' --url localhost:8525/message
+  curl -X POST -H "Content-Type: application/json" -d '{"title": "snowflake", "text": "Starting system update."}' --url localhost:8525/message
   if nixos-rebuild switch --flake . --impure; then
-    curl -X POST -H "Content-Type: application/json" -d '{"text": "Update successful."}' --url localhost:8525/message
+    curl -X POST -H "Content-Type: application/json" -d '{"title": "snowflake", "text": "Update successful."}' --url localhost:8525/message
   else
-    curl -X POST -H "Content-Type: application/json" -d '{"text": "Update failed!"}' --url localhost:8525/message
+    curl -X POST -H "Content-Type: application/json" -d '{"title": "snowflake", "text": "Update failed!"}' --url localhost:8525/message
   fi
 fi
 
