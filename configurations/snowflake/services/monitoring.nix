6e9ced6826712c15e306fd7309a10f629e015ca9
clemens <clemak27@mailbox.org>
2022-03-13 09:39:32 +0100

    chore(snowflake): Update prom/prometheus Docker tag to v2.33.5
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- configurations/snowflake/services/monitoring.nix
+++ configurations/snowflake/services/monitoring.nix
@@ -8,7 +8,7 @@ in
       prometheus =
         let
           service-name = "prometheus";
-          service-version = "v2.32.1"; # renovate: datasource=docker depName=prom/prometheus
+          service-version = "v2.33.5"; # renovate: datasource=docker depName=prom/prometheus
           service-port = "9090";
         in
         {
