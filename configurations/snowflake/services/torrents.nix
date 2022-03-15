17df0bb3354d0f9ac9ec839dad4170ba948f732a
clemens <clemak27@mailbox.org>
2022-03-15 17:38:24 +0100

    chore(snowflake): update linuxserver/jackett docker tag to v0.20.709
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- configurations/snowflake/services/torrents.nix
+++ configurations/snowflake/services/torrents.nix
@@ -43,7 +43,7 @@ in
       jackett =
         let
           service-name = "jackett";
-          service-version = "0.20.708"; # renovate: datasource=docker depName=linuxserver/jackett
+          service-version = "0.20.709"; # renovate: datasource=docker depName=linuxserver/jackett
           service-port = "9117";
         in
         {
