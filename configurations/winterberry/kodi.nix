51dcd63a3cdd5e8d64c1f82b63f995226bc4f100
clemak27 <clemak27@mailbox.org>
2022-04-17 15:45:37 +0200

    fix(winterberry): Use xorg kodi

--- configurations/winterberry/kodi.nix
+++ configurations/winterberry/kodi.nix
@@ -1,10 +1,12 @@
 { config, pkgs, lib, ... }:
 {
+  services.xserver.enable = true;
+  services.xserver.desktopManager.kodi.enable = true;
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "kodi";
+
   # Define a user account
   users.extraUsers.kodi.isNormalUser = true;
-  services.cage.user = "kodi";
-  services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
-  services.cage.enable = true;
 
   environment.systemPackages = [
     (pkgs.kodi.passthru.withPackages (kodiPkgs: with kodiPkgs; [
