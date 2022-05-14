b32d903b3f680c9019e82a238de295ebe069c6a0
clemak27 <clemak27@mailbox.org>
2022-05-14 11:12:43 +0200

    feat: add home-manager git submodule

--- configurations/winterberry/home.nix
+++ configurations/winterberry/home.nix
@@ -1,21 +1,21 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports = [
-    ../../home-manager/homecfg.nix
-  ];
+  # imports = [
+  #   ../../home-manager/homecfg.nix
+  # ];
 
-  homecfg = {
-    git = {
-      enable = true;
-      user = "clemak27";
-      email = "clemak27@mailbox.org";
-    };
-    nvim.enable = false;
-    tmux.enable = false;
-    tools.enable = true;
-    zsh.enable = true;
-  };
+  # homecfg = {
+  #   git = {
+  #     enable = true;
+  #     user = "clemak27";
+  #     email = "clemak27@mailbox.org";
+  #   };
+  #   nvim.enable = false;
+  #   tmux.enable = false;
+  #   tools.enable = true;
+  #   zsh.enable = true;
+  # };
 
   home.file."gitops-upgrade.sh".source = ./gitops-upgrade.sh;
 
