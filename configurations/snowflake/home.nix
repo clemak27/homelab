555832cab7b22d50a5b79d9d7744ff06ffaccfa5
clemak27 <clemak27@mailbox.org>
2022-05-14 11:11:31 +0200

    feat: remove home-manager

--- configurations/snowflake/home.nix
+++ configurations/snowflake/home.nix
@@ -1,21 +1,20 @@
 { config, pkgs, lib, ... }:
-
 {
-  imports = [
-    ../../home-manager/homecfg.nix
-  ];
+  # imports = [
+  #   ../../home-manager/homecfg.nix
+  # ];
 
-  homecfg = {
-    git = {
-      enable = true;
-      user = "clemak27";
-      email = "clemak27@mailbox.org";
-    };
-    nvim.enable = true;
-    tmux.enable = true;
-    tools.enable = true;
-    zsh.enable = true;
-  };
+  # homecfg = {
+  #   git = {
+  #     enable = true;
+  #     user = "clemak27";
+  #     email = "clemak27@mailbox.org";
+  #   };
+  #   nvim.enable = true;
+  #   tmux.enable = true;
+  #   tools.enable = true;
+  #   zsh.enable = true;
+  # };
 
   programs.zsh = {
     shellAliases = builtins.listToAttrs (
