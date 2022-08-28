dd5a2635715a748145bcdfe72b369933952ae22e
clemak27 <clemak27@mailbox.org>
2022-08-28 11:46:23 +0200

    feat: create networks outside of compose

--- services/dco.sh
+++ services/dco.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 if [[ $(hostname) == "toolbox" ]]; then
+  docker_cmd="/usr/bin/flatpak-spawn --host docker"
   docker_compose_cmd="/usr/bin/flatpak-spawn --host docker-compose"
 else
+  docker_cmd="docker"
   docker_compose_cmd="docker-compose"
 fi
 
 function __up() {
+  $docker_cmd network create private
+  $docker_cmd network create internal
+  $docker_cmd network create public
   $docker_compose_cmd -f homer/docker-compose.yml up -d
 }
 
