034e16f41e5ff30e74ffa8a870216ef6dcdc2c94
clemak27 <clemak27@mailbox.org>
2022-08-28 10:13:21 +0200

    chore: remove useless file

--- scripts/postInstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/Projects
-cd Projects || exit 1
-git clone https://github.com/clemak27/homelab.git
-cd homelab || exit 1
-toolbox create nix
-toolbox run -c nix ~/Projects/homelab/init_nix_toolbox.sh
