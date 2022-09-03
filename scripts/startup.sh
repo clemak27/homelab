3ccbcea70ffa13d35f08853a3c5830115e7d9c46
clemak27 <clemak27@mailbox.org>
2022-09-03 09:54:52 +0200

    feat: start script depolys services

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,5 +7,8 @@ then
   mkdir -p $project_dir
   cd $project_dir && git clone https://github.com/clemak27/homelab
   cd $project_dir/homelab && git checkout homelab2
+  # TODO add age keys.txt
   chown -R clemens:clemens $project_dir
 fi
+
+su clemens -c "cd $project_dir/homelab/services && ./deploy.sh" 
