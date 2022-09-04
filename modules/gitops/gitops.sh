cb3b0eeccf94547d319db6eac93ba2fd2c3403ef
clemak27 <clemak27@mailbox.org>
2022-09-04 19:17:41 +0200

    feat: gitops handles init deployment

--- modules/gitops/gitops.sh
+++ modules/gitops/gitops.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 git_cmd="git -C /var/home/clemens/Projects/homelab"
-message_cmd="curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message"
-update_cmd="cd /var/home/clemens/Projects/homelab/services && ./deploy.sh"
 title=$(hostname)
+
+if [ ! -f "/tmp/git_status_old" ]
+then
+  cd /var/home/clemens/Projects/homelab/services || exit 1
+  ./deploy.sh
+  curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Initial deployment done!\"}"
+fi
 
 $git_cmd pull --rebase
 
@@ -12,14 +17,13 @@ $git_cmd log --oneline > /tmp/git_status_new
 DIFF=$(cmp /tmp/git_status_old /tmp/git_status_new)
 if [ "$DIFF" != "" ]
 then
-  echo "$update_cmd"
-  "$update_cmd"
-  # "$message_cmd" -d "{\"title\": \"$title\", \"text\": \"Starting deployment.\"}"
-  # if $update_cmd; then
-  #   "$message_cmd" -d "{\"title\": \"$title\", \"text\": \"Update successful.\"}"
-  # else
-  #   "$message_cmd" -d "{\"title\": \"$title\", \"text\": \"Update failed!\"}"
-  # fi
+  cd /var/home/clemens/Projects/homelab/services || exit 1
+  curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Starting deployment.\"}"
+  if ./deploy.sh; then
+    curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Update successful.\"}"
+  else
+    curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Update failed!\"}"
+  fi
 fi
 
 mv /tmp/git_status_new /tmp/git_status_old
