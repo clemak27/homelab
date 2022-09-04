621cd6b9083389f23e975c00bd5c5fe1e63ff830
clemak27 <clemak27@mailbox.org>
2022-09-04 12:24:57 +0200

    feat: change init to run on first reboot only

--- modules/init/init.sh
+++ modules/init/init.sh
@@ -4,12 +4,13 @@ project_dir=/var/home/clemens/Projects
 
 if [ ! -d "$project_dir/homelab" ]
 then
+  echo "Checking out homelab repo..."
   mkdir -p $project_dir
   cd $project_dir && git clone https://github.com/clemak27/homelab
   cd $project_dir/homelab && git checkout homelab2
   # TODO add age keys.txt
   chown -R clemens:clemens $project_dir
+  echo "Done"
 fi
 
-su clemens -c "cd $project_dir/homelab/services && ./deploy.sh"
-
+# su clemens -c "cd $project_dir/homelab/services && ./deploy.sh"
