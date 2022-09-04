c4a5c2539be1c28fa38777841aa372650b55379f
clemak27 <clemak27@mailbox.org>
2022-09-04 08:23:13 +0200

    feat: add mp3gain_update as container

---
+++ services/mp3gain_update/src/mp3gain_update.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tree /app/music > /tmp/tree_new
+
+DIFF=$(cmp /tmp/tree_old /tmp/tree_new)
+if [ "$DIFF" != "" ]
+then
+  readarray -d '' FOLDER < <(find "/app/music/" -type d -print0)
+
+  for i in "${FOLDER[@]}"
+  do
+    cd "$i" || exit 1
+    fd mp3 . -d 1 -X mp3gain -a -p -k
+    cd /app/music || exit 1
+  done
+  curl -X POST -H "Content-Type: application/json" --url localhost:8525/message -d '{"text": "mp3gain update finished."}'
+fi
+
+mv /tmp/tree_new /tmp/tree_old
