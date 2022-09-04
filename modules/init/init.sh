755ee7f8aa5256f9443b6d6b5484a1a5ff57773d
clemak27 <clemak27@mailbox.org>
2022-09-04 17:08:56 +0200

    feat: improve age key handling

--- modules/init/init.sh
+++ modules/init/init.sh
@@ -2,13 +2,17 @@
 
 project_dir=/var/home/clemens/Projects
 
+# fix permissions
+# ignition creates the keys.txt file, but the dir is owned by root
+chown -R clemens:clemens /var/home/clemens/.config
+
+# git repo
 if [ ! -d "$project_dir/homelab" ]
 then
   echo "Checking out homelab repo..."
   mkdir -p $project_dir
   cd $project_dir && git clone https://github.com/clemak27/homelab
   cd $project_dir/homelab && git checkout homelab2
-  # TODO add age keys.txt
   chown -R clemens:clemens $project_dir
   echo "Done"
 fi
