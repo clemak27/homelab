4036a58ba09160e607c94d16179466358d27e5f7
clemak27 <clemak27@mailbox.org>
2022-09-07 15:06:04 +0200

    feat: gitops now prints also on first start

--- modules/gitops/gitops.sh
+++ modules/gitops/gitops.sh
@@ -3,14 +3,22 @@
 git_cmd="git -C /var/home/clemens/Projects/homelab"
 title=$(hostname)
 
+__deploy() {
+  cd /var/home/clemens/Projects/homelab/services || exit 1
+  curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Starting deployment.\"}"
+  if ./deploy.sh; then
+    curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Deployment successful.\"}"
+  else
+    curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Deployment failed!\"}"
+  fi
+}
+
 if [ ! -f "/tmp/git_status_old" ]
 then
-  cd /var/home/clemens/Projects/homelab/services || exit 1
   touch /var/mnt/data0/docker/traefik/traefik.log
   touch /var/mnt/data0/docker/traefik/acme.json
   touch /var/mnt/data0/docker/home-assistant/bumper-certs/custom_ca.pem
-  ./deploy.sh
-  curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Services deployed!\"}"
+  __deploy
 fi
 
 $git_cmd pull --rebase
@@ -20,13 +28,7 @@ $git_cmd log --oneline > /tmp/git_status_new
 DIFF=$(cmp /tmp/git_status_old /tmp/git_status_new)
 if [ "$DIFF" != "" ]
 then
-  cd /var/home/clemens/Projects/homelab/services || exit 1
-  curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Starting deployment.\"}"
-  if ./deploy.sh; then
-    curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Deployment successful.\"}"
-  else
-    curl -X POST -H 'Content-Type: application/json' --url 192.168.178.100:8525/message -d "{\"title\": \"$title\", \"text\": \"Deployment failed!\"}"
-  fi
+  __deploy
 fi
 
 mv /tmp/git_status_new /tmp/git_status_old
