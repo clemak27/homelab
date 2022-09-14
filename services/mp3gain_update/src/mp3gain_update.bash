070255327d02e02146ccaef0b7481e3c77350e7a
clemak27 <clemak27@mailbox.org>
2022-09-14 08:21:30 +0200

    fix: mp3gain_update restarts

--- services/mp3gain_update/src/mp3gain_update.bash
+++ services/mp3gain_update/src/mp3gain_update.bash
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-tree /app/music > /tmp/tree_new
+__mp3gu() {
+  tree /app/music > /tmp/tree_new
 
-DIFF=$(cmp /tmp/tree_old /tmp/tree_new)
-if [ "$DIFF" != "" ]
-then
-  readarray -d '' FOLDER < <(find "/app/music/" -type d -print0)
+  DIFF=$(cmp /tmp/tree_old /tmp/tree_new)
+  if [ "$DIFF" != "" ]
+  then
+    readarray -d '' FOLDER < <(find "/app/music/" -type d -print0)
 
-  for i in "${FOLDER[@]}"
-  do
-    cd "$i" || exit 1
-    fd mp3 . -d 1 -X mp3gain -a -p -k
-    cd /app/music || exit 1
-  done
-  curl -X POST -H "Content-Type: application/json" --url localhost:8525/message -d '{"title": "mp3gain"," text": "Update finished."}'
-fi
+    for i in "${FOLDER[@]}"
+    do
+      cd "$i" || exit 1
+      fd mp3 . -d 1 -X mp3gain -a -p -k
+      cd /app/music || exit 1
+    done
+    curl -X POST -H "Content-Type: application/json" --url 192.168.178.100:8525/message/silent -d '{"title": "mp3gain"," text": "Update finished."}'
+  fi
 
-mv /tmp/tree_new /tmp/tree_old
+  mv /tmp/tree_new /tmp/tree_old
+}
 
-# TODO replace with cronjob/inotify
-sleep 600
+while :
+do
+  # TODO replace with cronjob/inotify
+  __mp3gu
+  sleep 600
+done
+
