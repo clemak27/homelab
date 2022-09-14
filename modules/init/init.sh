5bfd525db88093528e8b27f6c5701e7b071ad632
clemak27 <clemak27@mailbox.org>
2022-09-14 22:40:20 +0200

    feat: prepare for merging PR

--- modules/init/init.sh
+++ modules/init/init.sh
@@ -12,7 +12,6 @@ then
   echo "Checking out homelab repo..."
   mkdir -p $project_dir
   cd $project_dir && git clone https://github.com/clemak27/homelab
-  cd $project_dir/homelab && git checkout homelab2
   chown -R clemens:clemens $project_dir
   echo "Done"
 fi
