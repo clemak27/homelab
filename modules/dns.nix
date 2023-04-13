9d2cf2b76df06d68f7978d7e714a0dd90d9ea006
clemak27 <clemak27@mailbox.org>
2023-04-13 21:33:08 +0200

    fix: try adding entries to hosts

--- modules/dns.nix
+++ modules/dns.nix
@@ -31,6 +31,28 @@
   #   '';
   # };
 
+  networking.extraHosts = ''
+    192.168.178.100 argocd.wallstreet30.cc
+    192.168.178.100 deconz.wallstreet30.cc
+    192.168.178.100 filebrowser.wallstreet30.cc
+    192.168.178.100 gitea.wallstreet30.cc
+    192.168.178.100 home-assistant.wallstreet30.cc
+    192.168.178.100 jackett.wallstreet30.cc
+    192.168.178.100 jellyfin.wallstreet30.cc
+    192.168.178.100 k3d.wallstreet30.cc
+    192.168.178.100 longhorn.wallstreet30.cc
+    192.168.178.100 miniflux.wallstreet30.cc
+    192.168.178.100 qobuz-dl.wallstreet30.cc
+    192.168.178.100 radarr.wallstreet30.cc
+    192.168.178.100 recipes.wallstreet30.cc
+    192.168.178.100 registry.wallstreet30.cc
+    192.168.178.100 sonarr.wallstreet30.cc
+    192.168.178.100 syncthing.wallstreet30.cc
+    192.168.178.100 traefik.wallstreet30.cc
+    192.168.178.100 transmission.wallstreet30.cc
+    192.168.178.100 vaultwarden.wallstreet30.cc
+  '';
+
   environment.etc = {
     "hosts.d/homelab".text = ''
       192.168.178.100 argocd.wallstreet30.cc
