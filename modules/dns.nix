5b8d9554bb505dfeadc476b9c117407aaa36dc05
clemak27 <clemak27@mailbox.org>
2023-07-30 12:12:55 +0200

    feat: make dns config more dynamic

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,10 +1,14 @@
 { config, pkgs, ... }:
+let
+  dnsIP = "192.168.178.100";
+  lbIP = "192.168.178.100";
+in
 {
   services.dnsmasq = {
     enable = true;
     settings = {
       interface = "enp3s0,wg0";
-      listen-address = "::1,127.0.0.1,192.168.178.100";
+      listen-address = "::1,127.0.0.1,${dnsIP}";
       server = [
         "1.1.1.1"
       ];
@@ -12,29 +16,29 @@
   };
 
   networking.extraHosts = ''
-    192.168.178.100 argocd.wallstreet30.cc
-    192.168.178.100 deconz.wallstreet30.cc
-    192.168.178.100 filebrowser.wallstreet30.cc
-    192.168.178.100 gitea.wallstreet30.cc
-    192.168.178.100 grafana.wallstreet30.cc
-    192.168.178.100 home-assistant.wallstreet30.cc
-    192.168.178.100 jackett.wallstreet30.cc
-    192.168.178.100 jellyfin.wallstreet30.cc
-    192.168.178.100 k3d.wallstreet30.cc
-    192.168.178.100 longhorn.wallstreet30.cc
-    192.168.178.100 miniflux.wallstreet30.cc
-    192.168.178.100 navidrome.wallstreet30.cc
-    192.168.178.100 prometheus.wallstreet30.cc
-    192.168.178.100 qobuz-dl.wallstreet30.cc
-    192.168.178.100 radarr.wallstreet30.cc
-    192.168.178.100 recipes.wallstreet30.cc
-    192.168.178.100 registry.wallstreet30.cc
-    192.168.178.100 sonarr.wallstreet30.cc
-    192.168.178.100 syncthing.wallstreet30.cc
-    192.168.178.100 traefik.wallstreet30.cc
-    192.168.178.100 transmission.wallstreet30.cc
-    192.168.178.100 vaultwarden.wallstreet30.cc
-    192.168.178.100 zigbee2mqtt.wallstreet30.cc
+    ${lbIP} argocd.wallstreet30.cc
+    ${lbIP} deconz.wallstreet30.cc
+    ${lbIP} filebrowser.wallstreet30.cc
+    ${lbIP} gitea.wallstreet30.cc
+    ${lbIP} grafana.wallstreet30.cc
+    ${lbIP} home-assistant.wallstreet30.cc
+    ${lbIP} jackett.wallstreet30.cc
+    ${lbIP} jellyfin.wallstreet30.cc
+    ${lbIP} k3d.wallstreet30.cc
+    ${lbIP} longhorn.wallstreet30.cc
+    ${lbIP} miniflux.wallstreet30.cc
+    ${lbIP} navidrome.wallstreet30.cc
+    ${lbIP} prometheus.wallstreet30.cc
+    ${lbIP} qobuz-dl.wallstreet30.cc
+    ${lbIP} radarr.wallstreet30.cc
+    ${lbIP} recipes.wallstreet30.cc
+    ${lbIP} registry.wallstreet30.cc
+    ${lbIP} sonarr.wallstreet30.cc
+    ${lbIP} syncthing.wallstreet30.cc
+    ${lbIP} traefik.wallstreet30.cc
+    ${lbIP} transmission.wallstreet30.cc
+    ${lbIP} vaultwarden.wallstreet30.cc
+    ${lbIP} zigbee2mqtt.wallstreet30.cc
   '';
 
   systemd.services.prometheus-dnsmasq-exporter = {
