5954059c08690e3273104026442843c5f8d75a3e
clemak27 <clemak27@mailbox.org>
2023-09-14 22:10:25 +0200

    chore: cleanup prepare_disk.sh

--- setup/prepare_disk.sh
+++ setup/prepare_disk.sh
@@ -3,7 +3,7 @@
 prepare_disk() {
   friendly_name="ssd"
   device="/dev/sdb"
-  passphrase="abcd"
+  passphrase="thisIsNotAGoodPassphrase"
 
   partition="${device}1"
   luksMapper="crypt${friendly_name}"
@@ -23,48 +23,11 @@ prepare_disk() {
   # create subvolumes
   mkdir -p /tmp/mnt
   mount "${unencryptedDisk}" /tmp/mnt
-  btrfs subvolume create /tmp/mnt/k3s
-  btrfs subvolume create /tmp/mnt/longhorn
+  btrfs subvolume create /tmp/mnt/media
+  btrfs subvolume create /tmp/mnt/backups
   umount /tmp/mnt
 
   cryptsetup luksClose ${unencryptedDisk}
 }
 
-mount_disk2() {
-  partition="/dev/vda1"
-  luksMapper="cryptData"
-  unencryptedDisk="/dev/mapper/${luksMapper}"
-
-  cryptsetup open "${partition}" "${luksMapper}"
-
-  mkdir -p /var/mnt/media
-  mkdir -p /var/mnt/backups
-
-  mount -o compress=zstd,subvol=media $unencryptedDisk /var/mnt/media
-  mount -o compress=zstd,subvol=backups $unencryptedDisk /var/mnt/backups
-
-  mkdir -p /var/nfs/media
-  mkdir -p /var/nfs/backups
-
-  mount --bind /var/mnt/media /var/nfs/media
-  mount --bind /var/mnt/backups /var/nfs/backups
-
-  # sudo systemctl start nfs-server.service
-}
-
-##!/bin/sh
-
-#set -e
-
-#while true ; do
-#uuid=$(journalctl -e | tail -n1 | grep -oP '(?<=orphaned pod \\")[^\\"]+')
-#echo "$uuid"
-#p="/var/lib/kubelet/pods/$uuid"
-## p="/var/lib/kubelet/pods/$uuid/volumes/*"
-## if [ -d "$p" ] ; then
-#  # echo "Moving $uuid"
-#  sudo mkdir -p /var/lib/kubelet/orphaned-pods/$uuid
-#  sudo mv "$p" /var/lib/kubelet/orphaned-pods/$uuid
-## fi
-#sleep 2
-#done
+prepare_disk
