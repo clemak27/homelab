9791c0418e72e9b1e43da4368e605f5bc74523b8
clemak27 <clemak27@mailbox.org>
2026-09-04 14:53:45 +0200

    fix(amd64): dhcp shouldn't be disabled

--- modules/hardware/amd64.nix
+++ modules/hardware/amd64.nix
@@ -24,7 +24,7 @@
         networking = {
           firewall.enable = false;
           networkmanager.enable = true;
-          useDHCP = false;
+          useDHCP = lib.mkDefault true;
         };
 
         boot.loader = {
